57a7073da8d825da8800161510bb183c918d20ea
AlexanderElizarev <elizarevdev@gmail.com>
2015-07-13 20:20:38 +0300

    Initial Commit

---
+++ InnopolisTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InnopolisTask
+//
+//  Created by АЛЕКСАНДР on 13.07.15.
+//  Copyright (c) 2015 UNICODE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
